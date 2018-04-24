758c2f22c3e8da3785041f3119cc90b460d5be20
Mark Van den Hoven <markvandenhoven@wcw-stud-145-109-5-161.wireless.uva.nl>
2018-04-24 10:20:49 +0200

    beetje opgeschoond

--- csv_files/classes.py
+++ csv_files/classes.py
@@ -1,42 +1,51 @@
 import csv
 
 class Lijnvoering:
-    def __init__(self, traject1, traject2 = '', traject3 = '', traject4 = '',
-    traject5 = '', traject6 = '', traject7= '', traject8= '', traject9 = ''):
-        self.traject1 = traject1
-        self.traject2 = traject2
-        self.traject3 = traject3
-        self.traject4 = traject4
-        self.traject5 = traject5
-        self.traject6 = traject6
-        self.traject7 = traject7
-        self.traject8 = traject8
-        self.traject9 = traject9
+    def __init__(self, traject1, traject2 = '',
+        traject3 = '', traject4 = '',traject5 = '',
+        traject6 = '', traject7= '', traject8= '',
+        traject9 = ''):
+            self.traject1 = traject1
+            self.traject2 = traject2
+            self.traject3 = traject3
+            self.traject4 = traject4
+            self.traject5 = traject5
+            self.traject6 = traject6
+            self.traject7 = traject7
+            self.traject8 = traject8
+            self.traject9 = traject9
 
     def SLijnvoering(self):
-        trajectLijst = [self.traject1, self.traject2, self.traject3, self.traject4,
-        self.traject5, self.traject6, self.traject7, self.traject8, self.traject9]
-        minuten = 0
-        treinen = 0
-        kritiekeSporen = []
-        for traject in trajectLijst:
-            if traject is not '':
-                minuten += Traject.minutenTraject(traject)
-                treinen += 1
-        for traject in trajectLijst:
-            if traject is not '':
-                for spoor in traject.__dict__.items():
-                    if spoor[1] is not '':
-                        if spoor[1] in kritiekeSporenLijst and spoor[1] not in kritiekeSporen:
-                            kritiekeSporen.append(spoor)
+        trajectLijst = [self.traject1, self.traject2,
+            self.traject3, self.traject4,self.traject5,
+            self.traject6, self.traject7, self.traject8,
+            self.traject9]
+            minuten = 0
+            treinen = 0
+            kritiekeSporen = []
+            for traject in trajectLijst:
+                if traject is not '':
+                    minuten += Traject.minutenTraject(traject)
+                    treinen += 1
+            for traject in trajectLijst:
+                if traject is not '':
+                    for spoor in traject.__dict__.items():
+                        if spoor[1] is not '':
+                            if (spoor[1] in kritiekeSporenLijst
+                                and spoor[1] not in kritiekeSporen):
+                                    kritiekeSporen.append(spoor)
 
         p = len(kritiekeSporen) / len(kritiekeSporenLijst)
 
-        return ['min = {}'.format(minuten), 't = {}'.format(treinen), 'p = {}'.format(p), 'S = {}'.format(10000 * p - (treinen * 20 + minuten / 10)) ]
+        return ['min = {}'.format(minuten),
+            't = {}'.format(treinen), 'p = {}'.format(p),
+            'S = {}'.format(10000 * p - (treinen * 20 + minuten / 10)) ]
 
 class Traject:
-    def __init__(self, spoor1, spoor2 = '', spoor3 = '', spoor4 = '',
-    spoor5 = '', spoor6 = '', spoor7= '', spoor8= '', spoor9 = ''):
+    def __init__(self, spoor1, spoor2 = '',
+        spoor3 = '', spoor4 = '',spoor5 = '',
+        spoor6 = '', spoor7= '', spoor8= '',
+        spoor9 = ''):
         self.spoor1 = spoor1
         self.spoor2 = spoor2
         self.spoor3 = spoor3
@@ -46,7 +55,8 @@ class Traject:
         self.spoor7 = spoor7
         self.spoor8 = spoor8
         self.spoor9 = spoor9
-        spoorlist = [self.spoor1, self.spoor2, self.spoor3, self.spoor4, self.spoor5,
+        spoorlist = [self.spoor1, self.spoor2,
+            self.spoor3, self.spoor4, self.spoor5,
             self.spoor6, self.spoor7, self.spoor8, self.spoor9]
         for i in range(len(spoorlist) - 1):
             if spoorlist[i] != '' and spoorlist[i+1] != '':
@@ -58,14 +68,15 @@ class Traject:
                         break
 
     def minutenTraject(self):
-        spoorlist = [self.spoor1, self.spoor2, self.spoor3, self.spoor4, self.spoor5,
-            self.spoor6, self.spoor7, self.spoor8, self.spoor9]
-        minuten = 0
-        for station in spoorlist:
-            if station is not '':
-                minuten += station.minuten
+        spoorlist = [self.spoor1, self.spoor2, self.spoor3,
+            self.spoor4, self.spoor5,self.spoor6,
+            self.spoor7, self.spoor8, self.spoor9]
+            minuten = 0
+            for station in spoorlist:
+                if station is not '':
+                    minuten += station.minuten
 
-        return minuten
+            return minuten
 
 class Spoor:
     def __init__(self, stationBegin, stationEind, minuten):
@@ -85,7 +96,8 @@ with open('StationsHolland.csv') as csvfile:
     stationLijst = []
     kritiekeStationLijst = []
     for station in stationsinfo:
-        stationLijst.append(Station(station[0], station[1], station[2], station[3]))
+        stationLijst.append(Station(station[0], station[1],
+            station[2], station[3]))
         if station[-1] == 'Kritiek':
             kritiekeStationLijst.append(station[0])
 
@@ -97,9 +109,16 @@ with open('ConnectiesHolland.csv') as csvfile:
     for lijn in stationsverbindingen:
         sporenLijst.append(Spoor(lijn[0], lijn[1], lijn[2]))
     for lijn in sporenLijst:
-        if (lijn.stationBegin in kritiekeStationLijst or lijn.stationEind in kritiekeStationLijst
-        and lijn not in kritiekeSporenLijst):
-            kritiekeSporenLijst.append(lijn)
+        if (lijn.stationBegin in kritiekeStationLijst
+            or lijn.stationEind in kritiekeStationLijst
+            and lijn not in kritiekeSporenLijst):
+                kritiekeSporenLijst.append(lijn)
+
+for i in range(len(sporenLijst)):
+    print('{}:{},{},{}'.format(i, sporenLijst[i].stationBegin,
+        sporenLijst[i].stationEind,
+        sporenLijst[i].minuten))
+
 
 traject_1 = Traject(sporenLijst[0], sporenLijst[1])
 
