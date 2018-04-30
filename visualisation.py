ce357cbd6c94516f5374a1fb6a2da7aa26df6df8
teunisvdh <teunisvdh#gmail.com>
2018-04-30 23:17:31 +0200

    Update

--- visualisation.py
+++ visualisation.py
@@ -7,17 +7,13 @@ import numpy as np
 from data import *
 from classes import classes
 from classes import helpers
+from classes import randomAlgoritme
 
 
-alkmaar = classes.Station('Alkmaar',52.63777924,4.739722252,'Kritiek')
+# alkmaar = classes.Station('Alkmaar',52.63777924,4.739722252,'Kritiek')
 
-x = alkmaar.x
-y = alkmaar.y
-
-# for i in range(20):
-# plt.plot(x, y, 'ro-')
-#
-# plt.show()
+# x = alkmaar.x
+# y = alkmaar.y
 
 with open('data/StationsHolland.csv') as csvfile:
     stationsinfo = csv.reader(csvfile, delimiter=',')
@@ -33,8 +29,14 @@ for station in stationList:
     plt.plot(x, y, 'ro-')
     plt.annotate(station.name, xy=(x,y))
 
+print(stationList[0].name)
+
+# plt.plot([52.3, 52.5], [3.6, 3.7], 'k-')
+# plt.plot([stationList[0].x,stationList[1].x], [stationList[0].y,stationList[1].y], 'k-')
 plt.xlabel("longitude")
 plt.ylabel("latitude")
 plt.title("Visualisation")
 plt.axis('equal')
 plt.show()
+
+print(stationList[0].name)
