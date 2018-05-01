59b1316d58f3678d95cdffe22fb52442013aaa3d
teunisvdh <teunisvdh#gmail.com>
2018-05-01 11:03:47 +0200

    stationconnect

--- visualisation.py
+++ visualisation.py
@@ -9,34 +9,35 @@ from classes import classes
 from classes import helpers
 from classes import randomAlgoritme
 
-
-# alkmaar = classes.Station('Alkmaar',52.63777924,4.739722252,'Kritiek')
-
-# x = alkmaar.x
-# y = alkmaar.y
-
 with open('data/StationsHolland.csv') as csvfile:
     stationsinfo = csv.reader(csvfile, delimiter=',')
     stationList = []
     for station in stationsinfo:
         stationList.append(classes.Station(station[0], station[1], station[2], station[3]))
 
-# print(station.name)
+stationTrajectory = [stationList[0], stationList[1], stationList[2], stationList[3]]
+
 for station in stationList:
     x = float(station.y)
     y = float(station.x)
-    # lin.line(x, y, 'ro-')
     plt.plot(x, y, 'ro-')
     plt.annotate(station.name, xy=(x,y))
 
-print(stationList[0].name)
+def connect(begin, eind):
+    x1 = float(stationList[begin].y)
+    y1 = float(stationList[begin].x)
+    x2 = float(stationList[eind].y)
+    y2 = float(stationList[eind].x)
+    plt.plot([x1, x2], [y1, y2], 'k-')
 
-# plt.plot([52.3, 52.5], [3.6, 3.7], 'k-')
-# plt.plot([stationList[0].x,stationList[1].x], [stationList[0].y,stationList[1].y], 'k-')
+connect(18, 10);
+
+for station in stationTrajectory:
+    print(station.name)
+    # connect(station, station+1)
+
 plt.xlabel("longitude")
 plt.ylabel("latitude")
 plt.title("Visualisation")
 plt.axis('equal')
 plt.show()
-
-print(stationList[0].name)
