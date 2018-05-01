9c4239612201166eec08d27216c7cb5fd184b9b1
Thymen <thymenyj@gmail.com>
2018-05-01 12:23:45 +0200

    Docstrings

--- classes/classes.py
+++ classes/classes.py
@@ -1,9 +1,28 @@
 class Line:
+"""
+This is a class which contains all trajectories (i.e. all railways used).
+
+Args:
+trajectoryList (list): list of trajectories.
+
+Functions:
+Sline(self,criticalRailwayList).
+"""
     def __init__(self, TrajectoryList):
         self.TrajectoryList = TrajectoryList
 
-    # calculates S for a set of Trajectorys
     def SLine(self, criticalRailwayList):
+    """
+    Function that determines S for Trajectory.
+
+    Args:
+    criticalRailwayList (list): List of all railways that are critical.
+
+    Returns:
+    Total amount of minutes (min), amount of trains used (t),
+    percentage of critical railways used (p) and the scorefunction
+    S = 10000*p - (t*20 + min/10).
+    """
         self.criticalRailwayList = criticalRailwayList
         minutes = 0
         trains = 0
@@ -29,6 +48,12 @@ class Line:
             'S = {}'.format(10000 * p - (trains * 20 + minutes / 10)) ]
 
 class Trajectory:
+"""
+Class which contains railways used.
+
+Args:
+raillist (list): List of railways.
+"""
     def __init__(self, Raillist):
         self.Raillist = Raillist
 
@@ -44,6 +69,15 @@ class Trajectory:
 
     # calculates total amount of minutes for each Trajectory
     def minutesTrajectory(self):
+    """
+    Function that calculates total minutes of trajectory
+    
+    Args:
+    raillist (list): List of railways.
+
+    Returns:
+    total minutes of trajectory.
+    """
         minutes = 0
         for station in self.Raillist:
                 minutes += station.minutes
@@ -52,12 +86,30 @@ class Trajectory:
         return minutes
 
 class Rail:
+"""
+Class of a rail between stations.
+
+Args:
+stationBeginning (str): Station where rail begins.
+stationEnd (str): Station where rail ends.
+minutes (int): Minutes needed to get from the beginning to end of the rail.
+"""
     def __init__(self, stationBeginning, stationEnd, minutes):
         self.stationBeginning = stationBeginning
         self.stationEnd = stationEnd
         self.minutes = int(minutes)
 
 class Station:
+"""
+Class of stations
+
+Args:
+name (str): Name of stations.
+x (float): x-coördinate of station's location.
+y (float): y-coördinate of station's location.
+Critical (str): Input 'Kritiek' when station is critical
+    and an empty string ('') when not critical.
+"""
     def __init__(self, name, x, y, Critical):
         self.name = name
         self.x = x
